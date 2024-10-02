94e28272d8d2f8ec64d10ef25f67e9e78894f646
Neeraj Kumar Nayak <neeraj.nayak003@gmail.com>
2024-10-02 19:01:53 +0530

    Create ChatbotRequest.java

---
+++ ChatbotRequest.java
@@ -0,0 +1,9 @@
+package com.example.feelgood.models;
+
+public class ChatbotRequest {
+    private String message;
+
+    public ChatbotRequest(String message) {
+        this.message = message;
+    }
+}
